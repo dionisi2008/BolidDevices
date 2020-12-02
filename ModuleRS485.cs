262364e38f6bdc738e35bcf5ffc24f21036ecd6a
Денис Калинин <dionisi2008@gmail.com>
2020-12-02 23:04:58 +0700

    Описание всего написаного, Основа будущих приборов

--- ModuleRS485.cs
+++ ModuleRS485.cs
@@ -1,21 +1,26 @@
-public class ModuleRS485
+public class ModuleRS485 //Модуль Интерфейса RS485 Для стандарной платы
 {
-    protected int Aress;
-    protected string NameDevices;
-    protected int SpeedIntefaces;
-    protected double VersionProgramm;
+    protected int Adress; //Адрес устройства в RS485
+    protected string NameDevices; //Имя Устройства
+    protected int SpeedIntefaces; //Скорость интерфейса
+    protected double VersionProgramm; //Версия прошивки устройства
+    protected byte[] MACDevices = new byte[8]; //Уникальный индификатор устройства
+    public delegate void DelegateWriteRS485(int AdressOut, int AdressGet, byte[] OutByte); 
+    //Экзимпляр делегаты записи в интерфейса
+    public DelegateWriteRS485 WriteRS485; //Для првязки всех устройств в интерфейсе
 
-    public delegate void DelegateWriteRS485(int AdressOut, int AdressGet, byte[] OutByte);
-
-    public DelegateWriteRS485 WriteRS485;
-    
-    public void ReadRS485(int AdressOut, int AdressGet, byte[] OutByte)
+    public void ReadRS485(int AdressOut, int AdressGet, byte[] OutByte) //Для првязки всех устройств в интерфейсе
     {
-        System.Console.WriteLine("DeBug: " + AdressOut + " " + AdressGet);
+        System.Console.WriteLine("DeBug: " + AdressOut + " " + AdressGet + " " + System.Text.UTF8Encoding.UTF8.GetString(MACDevices));
     }
 
-    public ModuleRS485(int NewAdress, string NameDevices, double GetVersionDevices, int GetSpeedDevices)
+    public ModuleRS485(int NewAdress, string NameDevices, double GetVersionDevices, int GetSpeedDevices) //Обявление Класса
     {
-        
+        this.Adress = NewAdress;
+        this.NameDevices = NameDevices;
+        this.VersionProgramm = GetVersionDevices;
+        this.SpeedIntefaces = GetSpeedDevices;
+        new System.Random(System.DateTime.Now.Millisecond).NextBytes(MACDevices);
+
     }
 }
