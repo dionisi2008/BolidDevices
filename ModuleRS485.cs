1b758fd510ff0a8d2c89441c0b59f41a7834feff
Денис Калинин <dionisi2008@gmail.com>
2020-12-06 00:02:03 +0700

    Именения от 06.12.2020 создал файл с фиксациец все выполненых изменений

--- ModuleRS485.cs
+++ ModuleRS485.cs
@@ -1,19 +1,63 @@
+using System.Collections.Generic;
+using System.Text;
+using System;
+using System.Timers;
 public class ModuleRS485 //Модуль Интерфейса RS485 Для стандарной платы
 {
     protected int Adress; //Адрес устройства в RS485
+
+    protected System.Timers.Timer RSTimer; //Таймер ожидания опроса ПКУ
     protected string NameDevices; //Имя Устройства
     protected int SpeedIntefaces; //Скорость интерфейса
+
+    private bool ConnectPKU = true; //Состояние подключения к ПКУ
     protected double VersionProgramm; //Версия прошивки устройства
     protected byte[] MACDevices = new byte[8]; //Уникальный индификатор устройства
-    public delegate void DelegateWriteRS485(int AdressOut, int AdressGet, byte[] OutByte); 
+    public delegate void DelegateWriteRS485(int AdressOut, int AdressGet, byte[] OutByte);
     //Экзимпляр делегаты записи в интерфейса
+
+    public delegate void DelegateCPUWriteEventRS485(byte[] GetDataRS485); //Делегата для общения модуля RS-485 c CPU
+    public DelegateCPUWriteEventRS485 CPUWriteDataRS; //Для записи в CPU
     public DelegateWriteRS485 WriteRS485; //Для првязки всех устройств в интерфейсе
 
-    public void ReadRS485(int AdressOut, int AdressGet, byte[] OutByte) //Для првязки всех устройств в интерфейсе
+    public void ReadRS485(int AdressOut, int AdressGet, byte[] OutByte) //Для чтения из интерфейса
     {
-        System.Console.WriteLine("DeBug: " + AdressOut + " " + AdressGet + " " + System.Text.UTF8Encoding.UTF8.GetString(MACDevices));
-    }
+        if (AdressOut == Adress) //Обращение именно к этому устройству
+        {
+            FuncConnectPKU(true); //Передача ЦПУ статус устоновки связи с ПКУ
 
+            // ЗАДЕЙСТВОВАТЬ!!!! CPUWriteDataRS(OutByte); // Передача в CPU
+
+            // System.Console.WriteLine(System.Text.UTF8Encoding.UTF8.GetString(OutByte));
+            // string[] GetInfoInterfacesRS485 = UTF8Encoding.UTF8.GetString(OutByte).Split('\r');
+            // switch (GetInfoInterfacesRS485[0])
+            // {
+            //     case "Поиск Устройства":
+            //     System.Console.WriteLine("Поиск Устройства " + Adress);
+            //     List<byte> OutDate = new List<byte>(UTF8Encoding.UTF8.GetBytes("Поиск Устройства" + '\r' + "Принято" + '\r'));
+            //     OutDate.AddRange(MACDevices);
+            //     WriteRS485.Invoke(Adress, AdressOut, OutDate.ToArray());
+            //     break;
+            // }
+        }
+    }
+    public void FuncConnectPKU(bool StateConnectGet) //Фукция оброботки события подключения / откючения ПКУ
+    {
+        if (StateConnectGet == false & ConnectPKU == true)
+        {
+            ConnectPKU = false;
+            System.Console.WriteLine("Нет связи с ПКУ");
+            //CPUWriteDataRS(UTF8Encoding.UTF8.GetBytes("Нет связи с ПКУ"));
+        }
+        else
+        {
+            RSTimer.Stop();
+            RSTimer.Start();
+            ConnectPKU = true;
+            System.Console.WriteLine("Cвязь с ПКУ востановлена");
+            //CPUWriteDataRS(UTF8Encoding.UTF8.GetBytes("Cвязь с ПКУ востановлена"));
+        }
+    }
     public ModuleRS485(int NewAdress, string NameDevices, double GetVersionDevices, int GetSpeedDevices) //Обявление Класса
     {
         this.Adress = NewAdress;
@@ -21,6 +65,17 @@ public class ModuleRS485 //Модуль Интерфейса RS485 Для ста
         this.VersionProgramm = GetVersionDevices;
         this.SpeedIntefaces = GetSpeedDevices;
         new System.Random(System.DateTime.Now.Millisecond).NextBytes(MACDevices);
+        this.RSTimer = new System.Timers.Timer(this.SpeedIntefaces);
+        this.RSTimer.Start();
+        this.RSTimer.Elapsed += (object sender, ElapsedEventArgs e) =>
+        {
+            if (ConnectPKU != false)
+            {
 
+                FuncConnectPKU(false);
+            }
+
+            System.Console.WriteLine("Debug Истёк таймер");
+        };
     }
 }
--- Program.cs
+++ Program.cs
@@ -17,12 +17,13 @@ namespace BolidDevices
 
             //Функция для добавления нового прибора
             //и прявязка его к пульту
-            NewDevicesRS485PKU(TestRS485, TestRS485[0], new ModuleRS485(1, "С2000-КДЛ", 2.27, 9600));
             NewDevicesRS485PKU(TestRS485, TestRS485[0], new ModuleRS485(2, "С2000-КДЛ", 2.27, 9600));
+            NewDevicesRS485PKU(TestRS485, TestRS485[0], new ModuleRS485(3, "С2000-КДЛ", 2.27, 9600));
 
 
             //Запись в от пульта в интерфейса данных
-            TestRS485[0].WriteRS485(1, 2, null);
+            TestRS485[0].WriteRS485(2, 1,System.Text.UTF8Encoding.UTF8.GetBytes("Поиск Устройства" + '\r' + ""));
+            System.Console.ReadLine();
         }
 
         public static void NewDevicesRS485PKU(List<ModuleRS485> GetRS485, ModuleRS485 GetPKU, ModuleRS485 GetNewDevices)
